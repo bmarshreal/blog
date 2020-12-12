5fa3634c039776d25dd7ee754d284a1969e27e97
bmarshreal <bmarshreal@yahoo.com>
2020-12-11 21:31:26 -0600

    Finish barebones of blog

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -10,6 +11,8 @@ const aboutContent =
   "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent =
   "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+const globalPostArr = [];
 
 const app = express();
 
@@ -19,10 +22,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res, next) => {
+  globalPostArr.forEach((post) => {});
   res.render("home", {
     startingContent: homeStartingContent,
+    posts: globalPostArr,
+    newpost: req.params.newpost,
+    // subTitle: subTitle,
+    // subBody: subBody,
   });
-  console.log(req.body);
 });
 
 app.get("/about", (req, res, next) => {
@@ -34,6 +41,36 @@ app.get("/about", (req, res, next) => {
 app.get("/contact", (req, res, next) => {
   res.render("contact", {
     contact: contactContent,
+  });
+});
+
+//What happens when someone wants to view our /compose route
+app.get("/compose", (req, res, next) => {
+  res.render("compose");
+});
+
+/*What happens when someone wants to send data from our /compose route 
+and how our server uses that data*/
+app.post("/compose", (req, res, next) => {
+  const post = {
+    title: req.body.postTitle,
+    body: req.body.postBody,
+  };
+  globalPostArr.push(post);
+  res.redirect("/");
+});
+
+app.get("/posts/:newpost", (req, res, next) => {
+  const newPost = _.lowerCase(req.params.newpost);
+  console.log(globalPostArr);
+  globalPostArr.forEach((post) => {
+    const storedTitle = _.lowerCase(post.title);
+    if (storedTitle === newPost) {
+      res.render("post", {
+        title: post.title,
+        body: post.body,
+      });
+    }
   });
 });
 
