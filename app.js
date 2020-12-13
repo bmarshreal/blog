6aa0bf14fd2869f1ba5fcd4b20fa02f993edd8cc
bmarshreal <bmarshreal@yahoo.com>
2020-12-12 22:11:35 -0600

    Adding mongoDB to blog

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
 
+/////////////////////////////
+/*-->Global Variables<--*/
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent =
@@ -16,19 +19,59 @@ const globalPostArr = [];
 
 const app = express();
 
+/*-->Global Variables<--*/
+
+/////////////////////////
+
+/*-->Database<--*/
+mongoose.connect("mongodb://localhost/blogDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const postSchema = new mongoose.Schema({
+  title: String,
+  body: String,
+});
+
+const Post = mongoose.model("Post", postSchema);
+
+const homePost = new Post({
+  title: "Home",
+  body: homeStartingContent,
+});
+
+const aboutPost = new Post({
+  title: "About",
+  body: aboutContent,
+});
+
+const contactPost = new Post({
+  title: "Contact",
+  body: contactContent,
+});
+
+const defaultItems = [homePost, aboutPost, contactPost];
+
+/*-->Database<--*/
+
+//////////////////////////////////
+
+/*-->Routes & Server<--*/
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res, next) => {
-  globalPostArr.forEach((post) => {});
-  res.render("home", {
-    startingContent: homeStartingContent,
-    posts: globalPostArr,
-    newpost: req.params.newpost,
-    // subTitle: subTitle,
-    // subBody: subBody,
+  Post.find({}, (err, posts) => {
+    posts.forEach((post) => {
+      console.log(post._id + "--->ADDED");
+    });
+    res.render("home", {
+      homeContent: homePost,
+      posts: posts,
+    });
   });
 });
 
@@ -52,25 +95,31 @@ app.get("/compose", (req, res, next) => {
 /*What happens when someone wants to send data from our /compose route 
 and how our server uses that data*/
 app.post("/compose", (req, res, next) => {
-  const post = {
+  const post = new Post({
     title: req.body.postTitle,
     body: req.body.postBody,
-  };
-  globalPostArr.push(post);
-  res.redirect("/");
+  });
+  post.save((err) => {
+    if (!err) {
+      res.redirect("/");
+    }
+  });
 });
 
 app.get("/posts/:newpost", (req, res, next) => {
-  const newPost = _.lowerCase(req.params.newpost);
-  console.log(globalPostArr);
-  globalPostArr.forEach((post) => {
-    const storedTitle = _.lowerCase(post.title);
-    if (storedTitle === newPost) {
-      res.render("post", {
-        title: post.title,
-        body: post.body,
-      });
-    }
+  Post.find({}, (err, result) => {
+    result.forEach((post) => {
+      console.log(post.id);
+      let newPost = req.params.newpost;
+      const storedTitle = post.title;
+      console.log(newPost);
+      if (newPost === post.id) {
+        res.render("post", {
+          title: post.title,
+          body: post.body,
+        });
+      }
+    });
   });
 });
 
